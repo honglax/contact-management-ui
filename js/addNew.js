9ff337145591f2e94beb4722c77e951381c789e6
Jason <jason@coders.tokyo>
2019-06-22 16:57:07 +0700

    First commit - Add all files

---
+++ js/addNew.js
@@ -0,0 +1,51 @@
+document.addEventListener('DOMContentLoaded', function() {
+    const url = 'http://localhost:9081/contacts';
+    const addNewBtn = document.getElementById('addNewBtn');
+    const backBtn = document.getElementById('backBtn');
+    const txtName = document.getElementById('txtName');
+    const txtAddress = document.getElementById('txtAddress');
+    const txtPhoneNum = document.getElementById('txtPhoneNum');
+
+    backBtn.addEventListener('click', backToHome);
+    addNewBtn.addEventListener('click', addNewContact);
+
+    function formValidation() {
+        // Fetch all the forms we want to apply custom Bootstrap validation styles to
+        const forms = document.getElementsByClassName('needs-validation');
+        let validation = Array.prototype.filter.call(forms, function(form) {
+            form.addEventListener('submit', function(event) {
+                if (form.checkValidity() === false) {
+                    event.preventDefault();
+                    event.stopPropagation();
+                }
+                form.classList.add('was-validated');
+            });
+        });
+    };
+
+    function addNewContact() {
+        const fullName = txtName.value;
+        const address = txtAddress.value;
+        const phoneNum = txtPhoneNum.value;
+
+        if ((!fullName) || (!address) || (!phoneNum)) {
+            formValidation();
+        } else {
+            let newContact = {
+                info: {
+                    name: fullName,
+                    address: address,
+                    phone: phoneNum
+                }
+            };
+            axios.post(url, newContact).then(function(response) {
+                alert('Add New Contact successfully.')
+            });
+            backToHome;
+        }
+    };
+
+    function backToHome(event) {
+        window.location.replace('./');
+    };
+});
---
+++ js/script.js
@@ -0,0 +1,42 @@
+'use strict';
+document.addEventListener('DOMContentLoaded', function() {
+    const contactTable = document.getElementById('contact-info');
+    const contactBody = contactTable.children[1];
+    let contacts = [];
+    const url = 'http://localhost:9081/contacts';
+    const editBtn = '<button type="button" class="btn btn-outline-info btn-sm" data-type="edtBtn">Edit Contact</button>';
+    const deleteBtn = '<button type="button" class="btn btn-outline-danger btn-sm" data-type="delBtn">Delete Contact</button>';
+    const addBtn = document.getElementById('addNewBtn');
+    
+    axios.get(url).then(function(response) {
+        contacts = response.data;
+        render(contacts);
+    });
+
+    contactBody.addEventListener('click', onContactClicked);
+    addBtn.addEventListener('click', function(event) {
+        window.location = './add-contact.html';
+    });
+   
+    function render(contacts) {
+        var content = contacts.map(function(contact) {
+            return '<tr data-id="' + contact.id + '"><th scope="row">' + (contacts.indexOf(contact) + 1) + '</th><td>' + contact.info.name + '</td><td>' + contact.info.address + '</td><td>' + contact.info.phone + '</td><td>' + editBtn +'</td><td>' + deleteBtn + '</td></tr>'
+        });
+        contactBody.innerHTML = content.join('');
+    };
+
+    function onContactClicked(event) {
+        var button = event.target;
+        if (button.localName != 'button') {
+            return;
+        }
+        var index = parseInt(button.parentNode.parentNode.dataset.id);
+        if (button.dataset.type == 'edtBtn') {
+            // console.l
+        }
+        if (button.dataset.type == 'delBtn') {
+            
+        }
+     }
+
+});
